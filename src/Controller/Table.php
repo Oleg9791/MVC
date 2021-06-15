493d7881d0e189a43ccc14f25175a083096cd19f
Oleg9791 <oleg9791@tut.by>
2021-06-15 23:30:16 +0300

    added MVC 15.06.2021

---
+++ src/Controller/Table.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use W1020\Table as ORMTable;
+use App\View\View;
+
+class Table
+{
+    protected ORMTable $model;
+    protected View $view;
+
+    public function __construct()
+    {
+        $config = include __DIR__ . "/../../Config.php";
+//        print_r($config);
+        $this->model = new ORMTable($config);
+        $this->view = new View();
+    }
+
+    public function show()
+    {
+//        echo "123";
+//        print_r($this->model->get());
+        $this
+            ->view
+            ->setData($this->model->get())
+            ->setTemplate("Table/show")
+            ->view();
+    }
+}
---
+++ templates/Table/show.php
@@ -0,0 +1,7 @@
+<?php
+
+use W1020\HTML\Table;
+
+ echo (new Table())->setData($this->data)->setClass("table table-success table-striped")->html();
+
+//print_r($this->data);
