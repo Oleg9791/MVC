5b8c8e8dc40864564290cad06ae29980f4361505
Oleg9791 <oleg9791@tut.by>
2021-06-23 16:20:47 +0300

    changes

--- templates/Main/index.php
+++ templates/Main/index.php
@@ -1,4 +1,4 @@
 <h1>Welcome!!!</h1>
-<a href="?type=Ved&action=show">Показать таблицу</a>
-<a href="?type=Gb&action=show">Показать таблицу Gb</a>
+<a href="?type=ved&action=show">Показать таблицу ved</a>
+<a href="?type=gb&action=show">Показать таблицу Gb</a>
 <a href="?type=opros&action=show">Показать таблицу opros</a>
